01efd760ec8e09e8120b6f899ff2285cd65fdb5e
Sahil Deshmukh <sahildeshmukh2898@gmail.com>
2025-10-19 13:36:40 +0530

    vercel config fix new port fix new

--- youtube-companion-backend/server.js
+++ youtube-companion-backend/server.js
@@ -57,7 +57,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
